c34016bc0cc9884a44259351d9f489585ca3591f
Anirudh7576 <gogikaranirudh.de18@gmail.com>
2026-08-25 19:18:29 +0530

    modified files

--- anonymus.py
+++ anonymus.py
@@ -1,5 +1,5 @@
 # local
-import csv
+# import csv
 
 # path = r"sales_table.csv"
 
@@ -9,13 +9,56 @@ import csv
 #     for row in reader:
 #         print(row)
 # ========================================================================
-# online csv url
-import pandas as pd
+# csv for data analysis
 
-path = r"https://raw.githubusercontent.com/softhints/Pandas-Exercises-Projects/refs/heads/main/data/europe_pop.csv"
+# import pandas as pd
 
-data = pd.read_csv(path)
-print(data)
+# path = r"https://raw.githubusercontent.com/softhints/Pandas-Exercises-Projects/refs/heads/main/data/europe_pop.csv"
+
+# data = pd.read_csv(path)
+# print(data)
+
+# =========================================================================
+# csv by requests
+
+# import requests as requests
+# from pathlib import Path
+
+# path = "https://raw.githubusercontent.com/softhints/Pandas-Exercises-Projects/refs/heads/main/data/europe_pop.csv"
+
+# response = requests.get(path)
+
+# print(response)
+# ==========================================================================
+# import requests as requests
+# from pathlib import Path
+
+# path = "https://raw.githubusercontent.com/softhints/Pandas-Exercises-Projects/refs/heads/main/data/europe_pop.csv"
+
+# response = requests.get(path)
+
+# with open(response, "w") as file:
+# print(response)
+# ============================================================================
+import csv
+import requests
+from io import StringIO
+
+url = "https://raw.githubusercontent.com/softhints/Pandas-Exercises-Projects/refs/heads/main/data/europe_pop.csv"
+
+response = requests.get(url)
+response.raise_for_status()
+
+reader = csv.reader(StringIO(response.text))
+output_path = r"C:/Users/Anirudh Gogikar/Downloads/csv_file_loading.csv"
+
+with open(output_path, "w", newline= "", encoding= "utf-8") as file:
+    writer = csv.writer(file)
+
+    for row in reader:
+        writer.writerow(row)
+print(row)
+
 
 
 
